974a92784744d37856cb413edd33e3962b94b34a
Rodolphe <rodolphebansard@yahoo.com>
2022-04-17 16:26:12 +0200

    parallax directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CultureBuiltMyBrandComponent } from './components/culture-built-my-brand/culture-built-my-brand.component';
 import { NeoplantsCloneComponent } from './components/neoplants-clone/neoplants-clone.component';
+import { ParallaxDirective } from './components/neoplants-clone/parallax.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CultureBuiltMyBrandComponent,
-    NeoplantsCloneComponent
+    NeoplantsCloneComponent,
+    ParallaxDirective
   ],
   imports: [
     BrowserModule,
